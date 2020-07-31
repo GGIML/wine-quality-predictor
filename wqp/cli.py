7ddaee19df9a3538447785736a1c619a9d0e95f3
GGIML <g.gimonnet@gmail.com>
2020-07-31 15:00:47 +0200

    Implemented cli wqp

---
+++ wqp/cli.py
@@ -0,0 +1,17 @@
+import click
+from wqp import __version__
+
+@click.group(name='wqp')
+@click.version_option(__version__)
+def wqp():
+    pass
+
+@wqp.group(name='jobs')
+def jobs():
+    pass
+
+@jobs.command(name='train')
+@click.option('--data-path', '-d')
+def train(data_path):
+    print(data_path)
+    pass
